89ca5c8141b44136d0c38ffcf867b4123c82b395
mmalikov <grelexis1337@inbox.ru>
2020-08-18 16:52:44 +0600

    Updates

---
+++ precache-manifest.b78becfa737c69eb1cd66e91bdd942c4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6927b726d7d556b4991a8f8bfbe952d",
+    "url": "/andante/index.html"
+  },
+  {
+    "revision": "612dc74268fbf6b7b93c",
+    "url": "/andante/static/css/main.0d27d850.chunk.css"
+  },
+  {
+    "revision": "4fdc7f6573b160290ea1",
+    "url": "/andante/static/js/2.02d83078.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andante/static/js/2.02d83078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612dc74268fbf6b7b93c",
+    "url": "/andante/static/js/main.7d299fb9.chunk.js"
+  },
+  {
+    "revision": "e57c2dab9d3e8cccbfdd",
+    "url": "/andante/static/js/runtime-main.94e5cd9d.js"
+  },
+  {
+    "revision": "58e1863f21ebfb06af2524bb49f2fedb",
+    "url": "/andante/static/media/ADNA_logo.58e1863f.png"
+  },
+  {
+    "revision": "a2331a4d98fbd55db08dffe573951ccd",
+    "url": "/andante/static/media/Bugday.a2331a4d.png"
+  },
+  {
+    "revision": "a258a2267d6aa522ae4ac85cc748c3d6",
+    "url": "/andante/static/media/TEMORARY.a258a226.png"
+  },
+  {
+    "revision": "b8cb813daa32593267528230072b5ad4",
+    "url": "/andante/static/media/aboutBg.b8cb813d.png"
+  },
+  {
+    "revision": "9aaf8295b8e809d3111e24f3b14dfdb6",
+    "url": "/andante/static/media/aboutUsBG.9aaf8295.png"
+  },
+  {
+    "revision": "602bdc717e5163a49eaee86c8ab4a6d8",
+    "url": "/andante/static/media/arrowLeft1.602bdc71.svg"
+  },
+  {
+    "revision": "1cd833619d8ca7eb0d2d41572ea3c689",
+    "url": "/andante/static/media/arrowRight1.1cd83361.svg"
+  },
+  {
+    "revision": "d97607f1a3b4e93106cecb152654e60e",
+    "url": "/andante/static/media/blueberries.d97607f1.png"
+  },
+  {
+    "revision": "19faf162c4a7e8ccf4cfa7fc3a4cfaac",
+    "url": "/andante/static/media/cake.19faf162.png"
+  },
+  {
+    "revision": "09f9af54af54d523bf04268d1159f683",
+    "url": "/andante/static/media/car.09f9af54.png"
+  },
+  {
+    "revision": "a262c81b343d8b03dec8d4bc1ee9f9a9",
+    "url": "/andante/static/media/cassata_slice.a262c81b.png"
+  },
+  {
+    "revision": "cad5f745be7f9a8a70ca302ac042aa8f",
+    "url": "/andante/static/media/chef_fog.cad5f745.png"
+  },
+  {
+    "revision": "83e99b2e5a86d669f38aceb186a7f10c",
+    "url": "/andante/static/media/chocolate.83e99b2e.png"
+  },
+  {
+    "revision": "94996d549107015263834a4f9b4f0cd1",
+    "url": "/andante/static/media/dairy.94996d54.png"
+  },
+  {
+    "revision": "2b3789c892b618331d6f43361ce41cb6",
+    "url": "/andante/static/media/donut_caramel.2b3789c8.png"
+  },
+  {
+    "revision": "0096c58847d8db61fb8bda75a465a932",
+    "url": "/andante/static/media/fog.0096c588.png"
+  },
+  {
+    "revision": "3ad682f5b8443d36d753532ad217e474",
+    "url": "/andante/static/media/fog_rev.3ad682f5.png"
+  },
+  {
+    "revision": "8bfb0b58ac4fa70124119731daafc539",
+    "url": "/andante/static/media/gazel.8bfb0b58.png"
+  },
+  {
+    "revision": "e94952aeb2b9177b1fb84c7326d20aff",
+    "url": "/andante/static/media/milk.e94952ae.png"
+  },
+  {
+    "revision": "d366bcbbbf7aa7fbecb15b736dcf9e30",
+    "url": "/andante/static/media/money.d366bcbb.png"
+  },
+  {
+    "revision": "078392c97f85892248333bade8ebde56",
+    "url": "/andante/static/media/nuts.078392c9.png"
+  },
+  {
+    "revision": "f5d27405ca82938ebe3c245d108084aa",
+    "url": "/andante/static/media/povar.f5d27405.png"
+  },
+  {
+    "revision": "3d71dd935b975e8c9a1b85af12b49481",
+    "url": "/andante/static/media/raspberry.3d71dd93.png"
+  },
+  {
+    "revision": "869a5babffb124a2d3ddf02c92a0c36f",
+    "url": "/andante/static/media/raspberry_catalog.869a5bab.png"
+  },
+  {
+    "revision": "b77ae0bdd34ad12fd7c7790415b1ce29",
+    "url": "/andante/static/media/springform_flachboden_zenker_creativestudio.b77ae0bd.png"
+  },
+  {
+    "revision": "96902cba930a592a3cafd11829aeef1d",
+    "url": "/andante/static/media/strawberry.96902cba.png"
+  },
+  {
+    "revision": "83d030f72db0d63935adc5e57f9736ff",
+    "url": "/andante/static/media/tray.83d030f7.png"
+  },
+  {
+    "revision": "5f1b3801a0fa16dca69ea31802bcea0a",
+    "url": "/andante/static/media/underHeader.5f1b3801.png"
+  },
+  {
+    "revision": "2bc947e5898eeb361197e8607c294b39",
+    "url": "/andante/static/media/watch.2bc947e5.png"
+  }
+]);
