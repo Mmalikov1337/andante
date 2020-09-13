3ebadc9f08791c2b1a64a3f51ea85f3946953425
Maksim Malikov <grelexis1337@inbox.ru>
2020-09-13 14:23:26 +0600

    add links to footer, fix link in slider

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,49 @@
 import React from 'react'
 import './Footer.scss'
+const MenuItem = (props) => {
+    // console.log('menu item')
+    return (
+        <li className='footer__menu__list__element'>
+            <a className="footer__menu__list__element_item" rel="stylesheet" href={props.href}>
+                {props.name}
+            </a>
+        </li>
+    )
+}
+
+const Menu = (props) => {
+    // console.log('menu')
+    return (
+        <ul className="footer__menu__list">
+            {props.items.map((item, index) =>
+                <MenuItem name={item.name} href={item.href} key={index} />
+            )}
+        </ul>
+    )
+}
+
+const MenuItems = [
+    {
+        name: 'О нас',
+        href: '#aboutUs'
+    },
+    {
+        name: 'Ассортимент',
+        href: '#assortment'
+    },
+    {
+        name: 'Отзывы',
+        href: '#recall'
+    },
+    {
+        name: 'Доставка',
+        href: '#delivery'
+    },
+    {
+        name: 'Контакты',
+        href: '#footer'
+    },
+]
 
 export default function Footer() {
     return (
@@ -11,40 +55,42 @@ export default function Footer() {
                     <p className='footer__menu__title'>
                         Меню:
                     </p>
-                    <ul className="footer__menu__list">
+
+                    {/* <ul className="footer__menu__list">
                         <li className="footer__menu__list__element">О нас</li>
                         <li className="footer__menu__list__element">Ассортимент</li>
                         <li className="footer__menu__list__element">Отзывы</li>
                         <li className="footer__menu__list__element">Доставка</li>
                         <li className="footer__menu__list__element">Политика конфиденциальности</li>
-                    </ul>
+                    </ul> */}
+                    <Menu items = {MenuItems}/>
                 </div>
-                
-                    <div className="footer__contacts">
-                        <p className='footer__contacts__title'>
-                            Контакты:
+
+                <div className="footer__contacts">
+                    <p className='footer__contacts__title'>
+                        Контакты:
                         </p>
-                        <p className="footer__contacts__mail">
-                            Почта для продажи:
+                    <p className="footer__contacts__mail">
+                        Почта для продажи:
                             <span>
-                                office@an-da.ru
+                            office@an-da.ru
                             </span>
-                        </p>
-                        <p className="footer__contacts__phone">
-                            Телефон для связи:
+                    </p>
+                    <p className="footer__contacts__phone">
+                        Телефон для связи:
                             <span>
-                                8 (3812) 58-05-44
+                            8 (3812) 58-05-44
                             </span>
-                        </p>
-                    </div>
-                    </div>
-                    <div className="footer__link">
-                        <a href="/catalog" target="_blank" rel="noopener noreferrer">
-                            <button className="footer__link__button btn-o">
-                                ПЕРЕЙТИ В КАТАЛОГ
+                    </p>
+                </div>
+            </div>
+            <div className="footer__link">
+                <a href="/catalog" target="_blank" rel="noopener noreferrer">
+                    <button className="footer__link__button btn-o">
+                        ПЕРЕЙТИ В КАТАЛОГ
                             </button>
-                        </a>
-                    </div>
+                </a>
+            </div>
             {/* </div> */}
 
             {/* </div> */}
